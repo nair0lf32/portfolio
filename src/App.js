af6f2271d3f23e4daa3574a8865178ee2bd5336e
nair0lf32 <florianedem@gmail.com>
2022-01-08 13:48:54 +0100

    minor changes

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to:
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Portfolio
         </a>
       </header>
     </div>
