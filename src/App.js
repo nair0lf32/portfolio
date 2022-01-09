a8547def1f9b0cc9687159c4c92901d656a5e035
nair0lf32 <florianedem@gmail.com>
2022-01-09 20:49:31 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to:
-        </p>
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Portfolio
-        </a>
-      </header>
-    </div>
-  );
+    <main className="">
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  )
 }
+
+
 
 export default App;
