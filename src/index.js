cdf4537732a65da9ba03215d82bcc1870f7fd470
nair0lf32 <florianedem@gmail.com>
2022-01-09 20:49:48 +0100

    update index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Styles/index.css';
 import App from './App';
+
+import { ThemeProvider } from './components/ThemeContext';
+import Background from './components/Background';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <Background>
+
+        <App />
+
+      </Background>
+    </ThemeProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
