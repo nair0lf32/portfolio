389cf489b5927d7fb6a5e161399bb4192b2feb3a
nair0lf32 <florianedem@gmail.com>
2022-01-09 20:59:08 +0100

    minor fix in index

--- src/index.js
+++ src/index.js
@@ -16,11 +16,10 @@ ReactDOM.render(
         <App />
 
       </Background>
-    </ThemeProvider>,
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
