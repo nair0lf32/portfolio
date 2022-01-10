a70781f27e0983d1aafa209e637528c2e2fb1ae8
nair0lf32 <florianedem@gmail.com>
2022-01-10 01:25:02 +0100

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,22 +8,29 @@ export default function Skills() {
             <div className="container px-5 py-10 mx-auto">
                 <div className="text-center mb-20">
                     <ChipIcon className="w-10 inline-block mb-4" />
-                    <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-                        Skills &amp; Technologies
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4">
+                        Skills and Technologies
                     </h1>
                     <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-                        ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-                        possimus est.
+                        Here are the technologies and tools I am confortable with. What I use to build my projects,
+                        And what I am passionate about.
                     </p>
                 </div>
-                <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                <div className="flex flex-wrap lg:w-2/5 sm:mx-auto sm:mb-2 -mx-2">
                     {skills.map((skill) => (
                         <div key={skill} className="p-2 sm:w-1/2 w-full">
                             <div className="bg-gray-800 rounded flex p-4 h-full items-center">
-                                <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                                <span className="title-font font-medium text-white">
-                                    {skill}
+
+                                <img
+                                    width="50"
+                                    height="50"
+                                    alt="logo"
+                                    className="object-cover left-0 top-0 mr-4"
+                                    src={skill.logo}
+                                />
+
+                                <span className="title-font font-medium text-white right-0">
+                                    {skill.name}
                                 </span>
                             </div>
                         </div>
