7d7e0002c4232158cb8164ec5741a21d029404fe
nair0lf32 <florianedem@gmail.com>
2022-01-10 13:12:38 +0100

    fix hover colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,13 +11,13 @@ export default function Navbar() {
                     </a>
                 </a>
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 font-medium flex flex-wrap items-center text-base justify-center">
-                    <a href="#projects" className="mr-5 hover:text-gray">
+                    <a href="#projects" className="mr-5 hover:text-green-400">
                         Projects
                     </a>
-                    <a href="#skills" className="mr-5 hover:text-white">
+                    <a href="#skills" className="mr-5 hover:text-green-400">
                         Skills
                     </a>
-                    <a href="#contact" className="mr-5 hover:text-white">
+                    <a href="#contact" className="mr-5 hover:text-green-400">
                         Contact
                     </a>
                 </nav>
