7c6b90a8b8c96a20d71bb1cc11693b0b0a429b21
nair0lf32 <florianedem@gmail.com>
2022-01-10 16:09:21 +0100

    tailwind configs update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,23 @@ module.exports = {
       center: true,
 
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        DEFAULT: '0.5rem',
+        sm: '1rem',
+        lg: '2rem',
+        xl: '3rem',
+        '2xl': '4rem',
       },
+
+      margin: {
+        '5px': '5px',
+      },
+
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['30px', '38px'],
+        xl: ['44px', '42px'],
+      }
 
     },
   },
