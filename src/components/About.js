d7c016cbaaeb1aace1a7d8d95c39dee4d8d44fbf
nair0lf32 <florianedem@gmail.com>
2024-01-07 01:07:28 +0100

    update about

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,28 @@
 import React from "react";
 import Fade from 'react-reveal/Fade';
 
-
 export default function About() {
     return (
         <section id="about">
-            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center sm:text-1xl md:text-2x1 xl:text-3x1 text-2xl">
-                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <div className="container mx-auto flex px-4 md:flex-row flex-col items-center sm:text-xl md:text-2xl xl:text-3xl text-lg">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-6 md:pr-4 flex flex-col md:items-start mb-4 md:mb-0 ml-4 md:ml-8">
                     <Fade top>
-                        <h1 className="title-font sm:text-5xl md:text-7x1 xl:text-9x1 text-6xl mb-4 font-medium" >
-                            Hi, I'm Florian,
+                        <h1 className="title-font sm:text-4xl md:text-5xl xl:text-5xl font-bold mb-2 md:mb-4">
+                            Hi, I'm Florian
                             <br className="hidden lg:inline-block" /> That medic who codes (somehow).
                         </h1>
                     </Fade>
                     <Fade left>
-                        <p className="mb-8 leading-relaxed" >
-                            I am a medical student from Cotonou (Benin), with some passion for software development. I use
+                        <p className="mb-4 leading-relaxed ml-4 md:ml-0">
+                            I am a Medical student from Cotonou (Benin), with some passion for software development. I use
                             multiple technologies, usually whatever does the job, with a personal liking for Python
-                            and Javascript. This website was built with React and TailwindCSS mostly for learning purposes.
-                            I am also invested in cybersecurity playing CTFs and learning about things you should not do
-                            while coding. I am a solo learner (selfmade club member) always looking for new challenges and
+                            and Javascript. React and TailwindCSS for instance, mostly for learning purposes.
+                            I am also invested in cybersecurity and good coding practices. I am a solo learner (selfmade club member) always looking for new challenges and
                             problems to solve (I don't want peace).
-                            This is my portfolio (as everyone got one now)
+                            This is my portfolio website (as everyone got one now)
                         </p>
                     </Fade>
-                    <div className="flex justify-center">
+                    <div className="flex justify-center md:justify-start mb-4">
                         <a
                             href="#contact"
                             className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
@@ -38,18 +36,16 @@ export default function About() {
                     </div>
                 </div>
 
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 z-50">
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 z-50 relative ml-4 mt-4 md:mt-0">
                     <Fade>
-                        <img width="300"
-                            height="300"
-                            className="object-cover object-center "
-                            alt="Hero florian edemessi self portrait"
-                            //Got broken links after deploying to github...could not load images locally
-                            //serving from remote then 
-                            //This is more a workaround than a fix...so TODO: fix this later
-                            src='/Assets/self.png'
-                            onMouseEnter={e => (e.currentTarget.src = '/Assets/self2.png')}
-                            onMouseLeave={e => (e.currentTarget.src = '/Assets/self.png')}
+                        <img
+                            width="400"
+                            height="400"
+                            className="object-cover object-center rounded-full"
+                            alt="florian edemessi hero"
+                            src="https://github.com/nair0lf32/portfolio/blob/3a8ce20ee267defa218f0cff1a5f80c3f5b7daec/public/Assets/self.png?raw=true"
+                            onMouseEnter={e => (e.currentTarget.src = "https://github.com/nair0lf32/portfolio/blob/3a8ce20ee267defa218f0cff1a5f80c3f5b7daec/public/Assets/self2.png?raw=true")}
+                            onMouseLeave={e => (e.currentTarget.src = "https://github.com/nair0lf32/portfolio/blob/3a8ce20ee267defa218f0cff1a5f80c3f5b7daec/public/Assets/self.png?raw=true")}
                         />
                     </Fade>
                 </div>
